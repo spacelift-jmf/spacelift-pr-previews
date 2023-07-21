3dda8f0d63a880a7def47165d4356579aa6b806f
Jean-Marc Fontaine <jmf@spacelift.io>
2023-07-21 15:31:33 -0400

    WIP

---
+++ manager/stacks/example-1/template/variables.tf
@@ -0,0 +1,8 @@
+variable "manager_stack_id" {}
+variable "infra_repository_branch" {}
+# variable "aws_role" {}
+# variable "certificate_arn" {}
+# variable "code_version" {}
+# variable "domain_name" {}
+variable "environment" {}
+variable "push_policy_id" {}
