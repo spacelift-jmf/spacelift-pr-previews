df0c3bf1397beffcb51ac6945a2e9d3ebeeb47c2
Jean-Marc Fontaine <jmf@spacelift.io>
2023-07-23 18:05:32 -0400

    WIP

--- manager/main.tf
+++
@@ -1,5 +0,0 @@
-# resource "spacelift_policy" "push" {
-#   name = "Demo Preview Environments Environment Push Policy"
-#   type = "GIT_PUSH"
-#   body = file("${path.module}/policies/push-policy.rego")
-# }
