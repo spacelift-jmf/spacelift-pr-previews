e5e9a34b9a6a5fdb648014422bdf1050275f39bd
Jean-Marc Fontaine <jmf@spacelift.io>
2023-07-23 17:25:38 -0400

    WIP

--- manager/monitored-stacks/simple-example/template/main.tf
+++ manager/monitored-stacks/simple-example/template/main.tf
@@ -3,72 +3,29 @@ resource "spacelift_stack" "preview" {
   autodeploy                   = true
   branch                       = var.branch
   github_action_deploy         = false
+  labels                       = ["pr-preview"]
   name                         = "Demo Preview Environments Example 1 ${var.preview_id}"
   project_root                 = "simple-example/"
   repository                   = "spacelift-pr-previews-example"
   space_id                     = "legacy"
   terraform_smart_sanitization = true
-
-  labels = [
-    "pr-preview",
-    #   "preview-environment-managed-by:${var.manager_stack_id}",
-    #   "preview_environment_id:${var.environment}"
-  ]
 }
 
 resource "spacelift_stack_destructor" "example_1" {
   stack_id = spacelift_stack.preview.id
 
-  # depends_on = [
-  # spacelift_aws_role.hello-service,
-  # spacelift_environment_variable.aws_role,
-  # spacelift_environment_variable.certificate_arn,
-  # spacelift_environment_variable.code_version,
-  # spacelift_environment_variable.domain_name,
-  # spacelift_environment_variable.environment,
-  # spacelift_policy_attachment.push,
-  # ]
+  depends_on = [
+    spacelift_environment_variable.pr_preview_id,
+    # spacelift_policy_attachment.push,
+  ]
 }
 
-# resource "spacelift_aws_role" "hello-service" {
-#   stack_id = spacelift_stack.hello-service.id
-#   role_arn = var.aws_role
-# }
-
-# resource "spacelift_environment_variable" "aws_role" {
-#   stack_id   = spacelift_stack.hello-service.id
-#   name       = "TF_VAR_aws_role"
-#   value      = var.aws_role
-#   write_only = false
-# }
-
-# resource "spacelift_environment_variable" "certificate_arn" {
-#   stack_id   = spacelift_stack.hello-service.id
-#   name       = "TF_VAR_certificate_arn"
-#   value      = var.certificate_arn
-#   write_only = false
-# }
-
-# resource "spacelift_environment_variable" "code_version" {
-#   stack_id   = spacelift_stack.hello-service.id
-#   name       = "TF_VAR_code_version"
-#   value      = var.code_version
-#   write_only = false
-# }
-
-# resource "spacelift_environment_variable" "domain_name" {
-#   stack_id   = spacelift_stack.hello-service.id
-#   name       = "TF_VAR_domain_name"
-#   value      = var.domain_name
-#   write_only = false
-# }
-
-# resource "spacelift_environment_variable" "environment" {
-#   name       = "TF_VAR_environment"
-#   stack_id   = spacelift_stack.example_1.id
-#   value      = var.environment
-#   write_only = false
-# }
+resource "spacelift_environment_variable" "pr_preview_id" {
+  name       = "TF_VAR_pr_preview_id"
+  stack_id   = spacelift_stack.preview.id
+  value      = var.preview_id
+  write_only = false
+}
 
 # resource "spacelift_policy_attachment" "push" {
 #   policy_id = var.push_policy_id
